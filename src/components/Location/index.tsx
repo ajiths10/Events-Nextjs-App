996040f9d5d119f0905000c75cda98c36f704859
ajiths10 <ajithsnair10@gmail.com>
2023-03-27 21:23:24 +0530

    Pin/Marker added to the map

--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -5,7 +5,10 @@ const LocationBody = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY as string,
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const center = useMemo(
+    () => ({ lat: 9.966264719076356, lng: 76.24227233899742 }),
+    []
+  );
 
   return (
     <div className="h-96 w-10/12 bg-gray-300 rounded-lg overflow-hidden">
@@ -15,8 +18,12 @@ const LocationBody = () => {
         <GoogleMap
           mapContainerClassName="h-full w-full"
           center={center}
-          zoom={10}
-        />
+          zoom={15}
+        >
+          <Marker
+            position={{ lat: 9.966264719076356, lng: 76.24227233899742 }}
+          />
+        </GoogleMap>
       )}
     </div>
   );
