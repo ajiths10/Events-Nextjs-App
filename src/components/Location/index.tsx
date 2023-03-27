ad7f81a24cd041a230529fdc7a1ee6a223a11dea
ajiths10 <ajithsnair10@gmail.com>
2023-03-27 21:42:21 +0530

    select area polygon style

--- src/components/Location/index.tsx
+++ src/components/Location/index.tsx
@@ -1,14 +1,36 @@
 import React, { useMemo } from "react";
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  Polygon,
+  useLoadScript,
+} from "@react-google-maps/api";
 
 const LocationBody = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY as string,
   });
-  const center = useMemo(
-    () => ({ lat: 9.966264719076356, lng: 76.24227233899742 }),
-    []
-  );
+  const center = useMemo(() => ({ lat: 24.886, lng: -70.268 }), []);
+
+  const paths = [
+    { lat: 25.774, lng: -80.19 },
+    { lat: 18.466, lng: -66.118 },
+    { lat: 32.321, lng: -64.757 },
+    { lat: 25.774, lng: -80.19 },
+  ];
+
+  const options = {
+    fillColor: "lightblue",
+    fillOpacity: 1,
+    strokeColor: "red",
+    strokeOpacity: 1,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    geodesic: false,
+    zIndex: 1,
+  };
 
   return (
     <div className="h-96 w-10/12 bg-gray-300 rounded-lg overflow-hidden">
@@ -18,11 +40,12 @@ const LocationBody = () => {
         <GoogleMap
           mapContainerClassName="h-full w-full"
           center={center}
-          zoom={15}
+          zoom={5}
         >
-          <Marker
+          {/* <Marker
             position={{ lat: 9.966264719076356, lng: 76.24227233899742 }}
-          />
+          /> */}
+          <Polygon paths={paths} options={options} />
         </GoogleMap>
       )}
     </div>
