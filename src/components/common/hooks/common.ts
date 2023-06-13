68b8d84916e581390a9d751136b1564f2d5681bf
Ajiths10 <ajithsnair10@gmail.com>
2023-06-13 21:54:56 +0530

    round number commmon function added

---
+++ src/components/common/hooks/common.ts
@@ -0,0 +1,7 @@
+export const roundNumbers = (value?: string | number) => {
+  if (!value) return "no data";
+
+  const valueAsNumber = Number(value);
+  if (valueAsNumber % 1 === 0) return valueAsNumber;
+  return Number(valueAsNumber).toFixed(2);
+};
