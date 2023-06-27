39fff69019f36c256663b640d2d2ab5135642490
Ajiths10 <ajithsnair10@gmail.com>
2023-06-27 22:34:09 +0530

    Deeplinking config file updated for the content type

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -9,6 +10,29 @@ const nextConfig = {
     BASE_URL: process.env.BASE_URL,
     SERVER_ENVIRONMENT: process.env.SERVER_ENVIRONMENT,
     REACT_APP_GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
+  },
+  // HEADER FUNCTION WILL DEFINE THE ROUTE CONTENT TYPE
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+      {
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
   },
 };
 
